5696f7b6af5a448ef9653f212b71592522d0e7fb
Manoj <manojgharti691@gmail.com>
2026-02-13 21:09:45 +0545

    to chech string is palindrome or not

--- Lecture17.py
+++ Lecture17.py
@@ -1,12 +1,31 @@
-def func(s,l,r):
-    while l<r:
-        if s[l] != s[r]:
-            return False
-        l+=1
-        r-=1
-    return True
+# def func(s,l,r):
+#     while l<r:
+#         if s[l] != s[r]:
+#             return False
+#         l+=1
+#         r-=1
+#     return True
     
+# string = input("Enter any string: ")
+# if func(string,0,len(string)-1) == True:
+#     print(f"The string {string} is palindrome.")
+
+
+
+
+
+def func(str: str,l,r):
+    if l>=r:
+        return
+    elif str[l] != str[r]:
+        return False
+    else:
+        func(str,l+1,r-1)
+        return True
+    
+
 string = input("Enter any string: ")
 if func(string,0,len(string)-1) == True:
-    print(f"The string {string} is palindrome.")
-    
+    print("The string is palindrome.")
+else:
+    print("The string is not palindrome.")
