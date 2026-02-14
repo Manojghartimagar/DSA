6426e018d1f4d60374a36384bbd6e15108422311
Manoj <manojgharti691@gmail.com>
2026-02-14 18:50:40 +0545

    Is sorted or not

---
+++ Lecture26.py
@@ -0,0 +1,17 @@
+#I use Bubble sort
+
+def bubble_sort(nums):
+    is_sort = False
+    for j in range(len(nums)-1):
+        if nums[j] >= nums[j+1]:
+            is_sort = True
+
+    if is_sort == False:
+        return True
+    
+    return False
+    
+if bubble_sort([1,2,3,4,5,6,9,7,8]) and True:
+    print("The list is sorted.")
+else:
+    print("List is not sorted.")
