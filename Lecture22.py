95bac6e5dd24920b2a9c6f83a1f950c878184b46
Manoj <manojgharti691@gmail.com>
2026-02-14 09:10:13 +0545

    Merge list

--- Lecture22.py
+++ Lecture22.py
@@ -13,11 +13,13 @@ def merge(left,right):
     if i < m:
         while i<m:
             result.append(left[i])
+            i+=1
         
     if j < n:
         while j<n:
             result.append(right[j])
+            j+=1
     
     return result
 
-print(merge([1,2,3,4,5],[6,7,8,9]))
+print(merge([1,2,3,4,5],[0,6,7,8,9]))
