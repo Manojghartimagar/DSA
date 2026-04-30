ea6528a1f97a0c6ed557109e096a02c7c1586d08
Manoj <manojgharti691@gmail.com>
2026-04-30 14:55:11 +0545

    Reverse an array using recursion

---
+++ Restart 2.0/video 16/part16.py
@@ -0,0 +1,10 @@
+# Reverse an Array Using Recursion
+num = [5,7,3,2,6,1,5,9]
+def reverse(l,r,num):
+    if l > r:
+        return num
+    num[l],num[r] = num[r],num[l]
+    return reverse(l+1,r-1,num)
+
+print(reverse(2,5,num))
+print(reverse(0,7,num))
