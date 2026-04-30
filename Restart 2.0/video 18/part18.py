266a90605378be03ae773785447f87ae7ec765d8
Manoj <manojgharti691@gmail.com>
2026-04-30 16:46:09 +0545

    Fibonacci numbers using recursiion

---
+++ Restart 2.0/video 18/part18.py
@@ -0,0 +1,9 @@
+# Find the fibonacci numbers using recursion
+def fibonacci(num):
+    if num == 0:
+        return 0
+    elif num == 1:
+        return 1
+    return fibonacci(num-2) + fibonacci(num-1)
+
+print(fibonacci(6))
