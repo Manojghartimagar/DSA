89c6f593b8f0a0d399055ece40baeb938624d8b5
Manoj <manojgharti691@gmail.com>
2026-05-05 18:03:11 +0545

    Linear Search

---
+++ Restart 2.0/video 31/Linear_Search.py
@@ -0,0 +1,8 @@
+def linear_search(nums,target):
+    for i in range(len(nums)):
+        if nums[i] == target:
+            return i
+        
+print(linear_search([5,3,9,8,1,6,4,-1,-100],4))
+
+# Time complexity = o(N) sc = o(1)
