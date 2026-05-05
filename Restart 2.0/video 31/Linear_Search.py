061f8bdd18f1fe48b6913dff15a7a5b734aa8244
Manoj <manojgharti691@gmail.com>
2026-05-05 18:05:57 +0545

    Linear Search

--- Restart 2.0/video 31/Linear_Search.py
+++ Restart 2.0/video 31/Linear_Search.py
@@ -2,6 +2,7 @@ def linear_search(nums,target):
     for i in range(len(nums)):
         if nums[i] == target:
             return i
+    return -1
         
 print(linear_search([5,3,9,8,1,6,4,-1,-100],4))
 
